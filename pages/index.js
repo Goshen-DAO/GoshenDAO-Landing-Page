3a883ee143bab2697defe0c8c2ea387f17bda7c3
dzypherit <31358319+dzypherit@users.noreply.github.com>
2024-01-27 18:48:18 +0800

    removed imported components and sections that are unused

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import { Footer } from '../components';
-import { About, Explore, Feedback, GetStarted, Hero, Insights, WhatsNew, World } from '../sections';
+
+import { About, Hero } from '../sections';
 
 const Home = () => (
   <div className="bg-primary-black overflow-hidden">
